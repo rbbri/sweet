a1d73a1351601a093968105390971557be27fa75
Alfie Freeman <runticle@gmail.com>
2018-11-13 14:44:57 +0000

    adds tests for test-sweet

---
+++ test-sweet.test.js
@@ -0,0 +1,34 @@
+const {
+  method,
+  it,
+  expect,
+  matchers
+} = require('./test-sweet')
+
+let executes = 0
+const noop = () => { executes += 1 }
+
+method('method', () => {
+  it('executes a callback function', () => {
+    const output= method('', noop)
+    expect(executes).toEqual(1)
+  })
+})
+
+method('expect', () => {
+  it('returns an object', () => {
+    const output= expect(true)
+    expect(typeof output).toEqual("object")
+    expect(typeof output.toEqual).toEqual("function")
+  })
+})
+
+method('matchers', () => {
+  method('toEqual', () => {
+    it('should be true when true', () => {
+      output = matchers(1).toEqual(1)
+      console.log(output)
+      expect(output).toEqual(true)
+    })
+  })
+})
